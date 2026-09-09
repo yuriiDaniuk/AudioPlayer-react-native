82fc4e8ed6119617f80c85874a97916b881dd541
Yurii Daniuk <danukura2007@gmail.com>
2026-09-09 18:44:40 +0300

    feat: add Header component to HomeScreen for improved layout

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
+import Header from '../components/Header'; 
 import TrackGrid from '../components/TrackGrid';
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 bg-transparent">
+      <Header />
       <TrackGrid />
     </View>
   );
